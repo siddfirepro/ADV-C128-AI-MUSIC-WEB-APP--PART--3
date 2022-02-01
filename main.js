626b485db8fd465ace699d2996404e29ec26764e
siddharth <93536983+siddfirepro@users.noreply.github.com>
2022-02-01 15:21:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+song="";
+rightwristx="";
+rightwristy="";
+leftwristy="";
+leftwristx="";
+function preload(){
+    song=loadSound("ocean blue.mp3");
+    song=loadSound("BTS.mp3");
+}
+function setup(){
+    canvas= createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();posenet=ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotposes);
+    }
+    function draw(){
+        image(video,0,0,600,500);
+    }
+    function play(){
+        song.play();
+        song.setVolume(1);
+        song.rate(1);
+    }
+    if(results.length>0){
+        console.log(results);
+leftwristx=results[0].pose.leftWrist.x;
+leftwristy=results[0].pose.leftWrist.y;
+console.log("leftwrist x= "+leftwristx+"leftwrist y="+leftwristy);
+
+rightwristx=results[0].pose.rightWrist.x;
+rightwristy=results[0].pose.rightWrist.y;
+console.log("rightwrist x= "+rightwristx+"rightwrist y="+rightwristy);
+    }
